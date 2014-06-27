4b401c85016c47cfe9f8800a37c1259b55d27d2a
Michael Patterson <mjpatter88@gmail.com>
2014-06-27 15:46:25 -0500

    Solution to second challenge added.

--- CoffeeTimeChallenge/Program.cs
+++ CoffeeTimeChallenge/Program.cs
@@ -16,9 +16,12 @@ namespace CoffeeTimeChallenge
         {
             System.Console.WriteLine("Challenge 1: Find three digits X, Y and Z such that XYZ in base10 (ten) is equal to ZYX in base9 (nine)? ");
             challengeOne();
+            System.Console.WriteLine("\nWrite 1,000,000 as the product of two numbers; neither of which contains any zeroes");
+            challengeTwo();
 
             System.Console.Read();
         }
+
 
         /// <summary>
         /// Find three digits X, Y and Z such that XYZ in base10 (ten) is equal to ZYX in base9 (nine)?
@@ -28,17 +31,11 @@ namespace CoffeeTimeChallenge
             //Assuming we start at 100 as the smallest 3-digit value
             for (int x = 440; x < 447; x++)
             {
-                //int baseNineVal = (x%10) + (((x/10)%10)*(9)) + ((x/100)*(81));
                 int baseNineVal = (x % 10) * (81) + (((x / 10) % 10) * (9)) + ((x / 100));
                 char a = x.ToString()[0];
                 char b = x.ToString()[1];
                 char c = x.ToString()[2];
                 string baseNineString = c.ToString() + b.ToString() + a.ToString();
-                //int a = (x % 10);
-                //int b = (((x / 10) % 10) * (9));
-                //int c = ((int)(x / 100) * 81);
-                //System.Console.WriteLine("a:" + a + " b:" + b + " c:" + c);
-                //System.Console.WriteLine(x + ": " + baseNineVal);
                 if(baseNineVal == x)
                 {
                     System.Console.WriteLine(x + "(in base 10) = " + baseNineString + "(in base 9)");
@@ -46,5 +43,24 @@ namespace CoffeeTimeChallenge
             }
         }
 
+        /// <summary>
+        /// Write 1,000,000 as the product of two numbers; neither of which contains any zeroes.
+        /// </summary>
+        private static void challengeTwo()
+        {
+            for (int x = 1; x < 1000; x++)
+            {
+                if (1000000 % x == 0)   //if x is a factor of 1,000,000
+                {
+                    int y = 1000000 / x;
+                    if((!x.ToString().Contains("0")) && (!y.ToString().Contains("0")))
+                    {
+                        System.Console.WriteLine(new StringBuilder(string.Format("{0}, {1}", x, y)));
+                    }
+                    
+                }
+            }
+        }
+        
     }
 }
